8dc604f933577d34e4559acc431ab4e3c14ca6f3
shot83428 <shot83428@gmail.com>
2022-07-04 10:43:52 +0800

    clear code

--- main.go
+++ main.go
@@ -80,32 +80,11 @@ func sqlconnect() {
 	}
 
 	//defer db.Close()
-	//SelectVersion()
+
 	ReadSample()
 	deleteSample("Jared")
 	UpdateSample("Nikita", 5468)
 	InsertSample("chinchin", 7777)
-}
-
-func SelectVersion() {
-	// Use background context
-	ctx := context.Background()
-
-	// Ping database to see if it's still alive.
-	// Important for handling network issues and long queries.
-	err := db.PingContext(ctx)
-	if err != nil {
-		log.Fatal("Error pinging database: " + err.Error())
-	}
-
-	var result string
-
-	// Run query and scan for result
-	err = db.QueryRowContext(ctx, "SELECT @@version").Scan(&result)
-	if err != nil {
-		log.Fatal("Scan failed:", err.Error())
-	}
-	fmt.Printf("%s\n", result)
 }
 
 func ReadSample() {
